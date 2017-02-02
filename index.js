40ef963ec81526a1418eebfff3b9cf6835cf9d89
jahnava <jahnavalmt@gmail.com>
2017-02-01 19:24:27 -0500

    created readme outlining user stories

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var server = express();
 var port = process.env.PORT || 8080;
 
 //to server css, images, and js
-server.use(express.static(__dirname + '/public'));
+server.use(express.static( __dirname + '/public'));
 server.get('/', function(request, response){
-  response.sendFile('index.html',{root: __dirname + 'public/html'});
+  response.sendFile('index.html',{root: __dirname + '/public/html'});
 });
 
 server.listen(port,function(){
