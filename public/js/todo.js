27ab1b56f349d67a1e7d34a301e11dbd55e9ebb3
jahnava <jahnavalmt@gmail.com>
2017-02-03 20:07:39 -0500

    Added the ability to gather all of the input field data into a single to do object. The render function was updated to use those objects to output the list items for the todolist, simulating persistent memory. Lastly, wrote the createTodo factory function to produce todo objects from a task, category, and date.

---
+++ public/js/todo.js
@@ -0,0 +1,28 @@
+//factory function
+function createTodo(task, date, category){
+  var todo = {};
+  todo.task = task; //setting and properties
+  todo.date = date;
+  todo.category = category;
+  return todo;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//var one = createTodo('get some bacon', '1/1/2017', 'groceries');
+//var two = createTodo('cry in the car', 'now', 'emotions');
+//console.log(one);
+//console.log(two);
